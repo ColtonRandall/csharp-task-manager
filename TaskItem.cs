0107874743137569b7bf23f6801bf7486e003887
Colton Randall <coltonrandall.nz@gmail.com>
2023-10-05 22:16:03 +1300

    Add initial task class

---
+++ TaskItem.cs
@@ -0,0 +1,19 @@
+﻿namespace task_manager
+{
+	public class TaskItem
+	{
+        // Variables that make up a task
+        private string _name;
+		private DateTime _date = new DateTime();
+		private bool _isCompleted;
+
+
+		public TaskItem(string name, DateTime date, bool isCompleted)
+		{
+			_name = name;
+			_date = date;
+			_isCompleted = isCompleted;
+		}
+	}
+}
+
